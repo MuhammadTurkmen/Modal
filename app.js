7b640e9d35c0c618f912803838c5d0ac9d4e3704
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-09 10:29:34 +0430

    init

--- app.js
+++ app.js
@@ -7,5 +7,6 @@ const modalOverlay = document.querySelector('.modal-overlay')
 const closeBtn = document.querySelector('.close-btn')
 
 modalBtn.addEventListener('click', function() {
-    
+    modalOverlay.classList.add('.open-modal')
 })
+
